3f68cc3179b5c9eea1cc45e57d0125ecef101bd4
Yoo Seungmin <dnjfht@gmail.com>
2024-12-14 03:22:27 +0900

    res.sendFile을 사용하여 이미지 파일 전송하기.

--- Node.js/Express.js/MVC_pattern/controllers/posts.controller.js
+++ Node.js/Express.js/MVC_pattern/controllers/posts.controller.js
@@ -1,7 +1,14 @@
 // post에 관한 로직을 처리
 
+const path = require("path");
+
 function getPost(req, res) {
-  res.send("<div><h1>Post Title</h1></div>");
+  //res.send("<div><h1>Post Title</h1></div>");
+  res.sendFile(
+    // path.join : 여러 세그먼트를 하나의 경로로 결합
+    // __dirname : 현재 실행하는 파일의 절대 경로
+    path.join(__dirname, "..", "public", "images", "cherry-juice.jpg")
+  );
 }
 
 module.exports = {
