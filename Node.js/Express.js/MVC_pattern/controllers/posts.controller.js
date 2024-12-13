94fb161f811ffadd8aa0609d55c37cd46448644a
Yoo Seungmin <dnjfht@gmail.com>
2024-12-14 04:46:17 +0900

    Template Layout을 사용하여 React SPA처럼 layout 구현하기.

--- Node.js/Express.js/MVC_pattern/controllers/posts.controller.js
+++ Node.js/Express.js/MVC_pattern/controllers/posts.controller.js
@@ -3,12 +3,17 @@
 const path = require("path");
 
 function getPost(req, res) {
-  //res.send("<div><h1>Post Title</h1></div>");
-  res.sendFile(
-    // path.join : 여러 세그먼트를 하나의 경로로 결합
-    // __dirname : 현재 실행하는 파일의 절대 경로
-    path.join(__dirname, "..", "public", "images", "cherry-juice.jpg")
-  );
+  // res.send("<div><h1>Post Title</h1></div>");
+
+  //res.sendFile(
+  // path.join : 여러 세그먼트를 하나의 경로로 결합
+  // __dirname : 현재 실행하는 파일의 절대 경로
+  // path.join(__dirname, "..", "public", "images", "cherry-juice.jpg")
+  // );
+
+  res.render("posts", {
+    templateName: "post",
+  });
 }
 
 module.exports = {
